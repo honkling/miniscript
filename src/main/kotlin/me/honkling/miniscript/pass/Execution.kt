1d6a0e072f04501deeebbd4d1fc86a676f9ebc7c
honkling <untitledgeese@gmail.com>
2025-11-29 23:01:00 -0600

    fix: delete vestigial execution pass

--- src/main/kotlin/me/honkling/miniscript/pass/Execution.kt
+++
@@ -1,9 +0,0 @@
-package me.honkling.miniscript.pass
-
-import me.honkling.miniscript.parser.ast.Block
-
-class Execution : Pass() {
-    override fun visitBlock(node: Block) {
-        node.execute()
-    }
-}
